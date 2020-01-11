dafd49c183465d13f438a5a5d4f001beeedd2cb0
CrioUser <crio-user@criodo.com>
2020-01-11 12:33:30 +0000

    module3

--- restaurants/pinterest_post.py
+++ restaurants/pinterest_post.py
@@ -1,9 +1,8 @@
 import base64
 import json
 import requests
-import os
 
-
+class Pinterest:
 #TODO: CRIO_TASK_MODULE_PINTEREST_SHARE
 # As part of this module you are expected to complete the publish_photo_msg function
 # Tasks:
@@ -27,44 +26,19 @@ import os
 #   1) message   -> string message to be posted
 #   2) image_url -> publicly accessible url of the image to be posted
 
-
-class Pinterest:
-    @staticmethod
-    def get_access_token(token_name):
-        access_token = os.getenv('PWD') + '/access_tokens.sh'
-        f = open(access_token, 'r+')
-        lines = f.readlines()
-        for line in lines:
-            tokens = line.strip().split('=')
-            if tokens[0] == token_name:
-                return tokens[1].strip()
-
-        return 'Not found'
-
-    def __init__(self):
-        self.access_token = Pinterest.get_access_token('PINTEREST_ACCESS_TOKEN')
-        
 # write your code below
-    
     def publish_photo_msg(self, message, image_url):
         URL="https://api.pinterest.com/v1/me/pins"
         PARAMS={
             "board":"arishh2/qeatsboard",
             "note":message,
             "image_url":image_url
-        }                
-        HEADERS={
-            "Authorization":self.access_token
         }
+        HEADERS={
+            "Authorization":"AoStRsMnNz43-c9vaPSm3QU9LFaiFejUlLPLp5BGdBlpGWCudApTQDAAAQv-RnQaM1igo1wAAAAA"
+        }
+
         r=requests.post(url=URL,params=PARAMS,headers=HEADERS)
         print(str(r.json()))
         
 
-if __name__ == '__main__':
-    pinterest = Pinterest()
-    
-    image_url = 'https://assetsds.cdnedge.bluemix.net/sites/default/files/styles/very_big_1/public/feature/images/ice_cream_1.jpg'
-    my_name = 'teens'
-
-    message = my_name + ' likes this ice-cream!'
-    pinterest.publish_photo_msg(message, image_url)
